a7e656df56bd529dbba879db99b633dccc9042e2
Hayato Iriumi <hiriumi@gmail.com>
2020-03-22 15:13:57 -0700

    Spring Flower

---
+++ youtube06.py
@@ -0,0 +1,22 @@
+import turtle
+turtle.setup(width=0.5, height=0.5)
+screen = turtle.Screen()
+screen.bgcolor("#000000")
+
+n=turtle.Pen()
+
+n.right(90)
+n.pensize(20)
+iro=['orchid', 'dark slate blue','blueviolet','mediumvioletred','hotpink','lightpink','darkviolet','dark slate blue','lavenderblush']
+n.speed(10)
+r=160
+for i in range (250):
+    color=iro[i%9]
+    n.pencolor(color)
+    n.circle(r)
+    r=r-0.7
+    n.right(40)
+    n.forward(50)
+    n.circle(1-i*1.4)
+
+turtle.exitonclick()
