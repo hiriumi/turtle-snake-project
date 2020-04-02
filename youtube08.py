a089606b3c7d5158218bb0b72ba442b6921a4902
Hayato Iriumi <hiriumi@gmail.com>
2020-04-02 16:51:28 -0700

    saving

---
+++ youtube08.py
@@ -0,0 +1,28 @@
+# Turtle Snake Project
+# URL
+
+import turtle
+    
+turtle.setup(width=0.5, height=0.5)
+screen = turtle.Screen()
+screen.bgcolor("#000000")
+
+t = turtle.Pen()
+
+iro=['limegreen','tomato', 'royalblue', 'gold']
+
+t.speed(10)
+
+for i in range (300):
+    t.pensize(7)
+    color=iro[i%4]
+    t.pencolor(color)
+    t.down()
+    t.forward(10+i)
+    t.forward(10+i)
+    t.up()
+    t.left(91)
+    t.forward(4+i)
+    t.right(30)
+
+turtle.exitonclick()
